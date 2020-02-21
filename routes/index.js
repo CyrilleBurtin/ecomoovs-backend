8f1a3d430ae2b7d243d393460d98a74b074fb84e
Cyrille <cyrille.burtin@gmail.gmail.com>
2020-02-21 09:01:39 +0100

    test route path /

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  console.log('index route')
+  res.render('index', { title: 'Ecomoovs' });
 });
 
 module.exports = router;
